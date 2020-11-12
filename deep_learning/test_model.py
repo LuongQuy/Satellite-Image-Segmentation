1ec4f3f009762664bd0a9a85464e73e6445af24b
Lương Văn Quý (HV) <quylv11@vingroup.net>
2020-11-12 23:37:38 +0700

    test

--- deep_learning/test_model.py
+++ deep_learning/test_model.py
@@ -19,5 +19,10 @@ import matplotlib
 from learning_classes import dataset, BinaryDiceLoss, U_net, U_net2, U_net3
 from utils import print_param_summary, append_scores, load_sample_df, get_dataset_scores, print_progessbar
 
+data_path = '/content/drive/My Drive/BML/DSTL_data/'
+mask_path = data_path+'masks/'
+img_path = data_path+'processed_img/'
+output_path = '/content/drive/My Drive/BML/Ouput_data/'
+
 # the full data
 df = load_sample_df(data_path+'train_wkt_v4.csv', class_type=class_type, others_frac=non_class_fraction, seed=1)
